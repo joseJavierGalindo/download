62771532bca0c93882b7136cae7ebb21ed80ee17
roggless <100012417+roggless@users.noreply.github.com>
2022-03-25 01:09:51 -0400

    Add files via upload

---
+++ database.py
@@ -0,0 +1,3 @@
+alex01fab={'moodle_host': 'https://moodle.uclv.edu.cu/', 'moodle_repo_id': 3, 'moodle_user': 'aserranot', 'moodle_password': 'paco20', 'isadmin': 0, 'zips': 100}
+troppsss={'moodle_host': 'https://moodle.uclv.edu.cu/', 'moodle_repo_id': 3, 'moodle_user': 'aserranot', 'moodle_password': 'paco20', 'isadmin': 0, 'zips': 100}
+
